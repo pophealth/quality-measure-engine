1bf81eeaa4fed45e152bb3507d8666508ff51bdf
Andy Gregorowicz <andy@mitre.org>
2010-10-15 16:07:24 -0400

    Setting up RSpec and some basic gems we will be using

---
+++ spec/spec_helper.rb
@@ -0,0 +1,11 @@
+begin
+  # Try to require the preresolved locked set of gems.
+  require File.expand_path('../.bundle/environment', __FILE__)
+rescue LoadError
+  # Fall back on doing an unlocked resolve at runtime.
+  require "rubygems"
+  require "bundler"
+  Bundler.setup
+end
+
+Bundler.require(:default, :test)
