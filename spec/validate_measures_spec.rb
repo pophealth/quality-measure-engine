29c4f9ad8849e9a797809f5d21dc47f2b1a54e60
Marc Hadley <mhadley@mitre.org>
2010-10-19 13:09:04 -0400

    Add tests for measure specs and sample patient records, fix resulting bugs in JSON files

---
+++ spec/validate_measures_spec.rb
@@ -0,0 +1,21 @@
+# Validate the measure specifications and patient samples
+
+require 'json'
+
+describe JSON, "All measure specifications" do
+  it 'should be valid JSON' do
+    Dir.glob('measures/*/*.json').each do |measure_file|
+      measure = File.read(measure_file)
+      json = JSON.parse(measure)
+    end
+  end
+end
+
+describe JSON, "All patient samples" do
+  it 'should be valid JSON' do
+    Dir.glob('measures/*/patients/*.json').each do |measure_file|
+      measure = File.read(measure_file)
+      json = JSON.parse(measure)
+    end
+  end
+end
