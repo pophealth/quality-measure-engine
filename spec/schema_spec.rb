abfe8c7db8c8d5d5e8e135827b7c802e49368ff8
Andy Gregorowicz <andy@mitre.org>
2010-10-20 12:35:21 -0400

    Creating a JSON Schema for measure results and validating it.

---
+++ spec/schema_spec.rb
@@ -0,0 +1,13 @@
+require 'json'
+
+describe JSON, "All JSON Schemas" do
+  it 'should conform to JSON Schema' do
+    schema = File.open("schema/schema.json", 'rb'){|f| JSON.parse(f.read)}
+    Dir.glob('schema/*.json').each do |schema_file|
+      if schema_file != 'schema/schema.json' # Don't check the schema itself
+       data = File.open(schema_file, 'rb'){|f| JSON.parse(f.read)}
+       JSON::Schema.validate(data, schema)
+      end
+    end
+  end
+end
