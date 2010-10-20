605b9c07ad4d3b648c18b93a17a98af7d0eaaac7
Andy Gregorowicz <andy@mitre.org>
2010-10-20 12:48:41 -0400

    Prefer single over double quotes

--- spec/schema_spec.rb
+++ spec/schema_spec.rb
@@ -1,8 +1,8 @@
 require 'json'
 
-describe JSON, "All JSON Schemas" do
+describe JSON, 'All JSON Schemas' do
   it 'should conform to JSON Schema' do
-    schema = File.open("schema/schema.json", 'rb'){|f| JSON.parse(f.read)}
+    schema = File.open('schema/schema.json', 'rb'){|f| JSON.parse(f.read)}
     Dir.glob('schema/*.json').each do |schema_file|
       if schema_file != 'schema/schema.json' # Don't check the schema itself
        data = File.open(schema_file, 'rb'){|f| JSON.parse(f.read)}
@@ -14,7 +14,7 @@ end
 
 describe JSON, 'Result Example' do 
   it 'should conform to the schema defined' do
-    schema = File.open("schema/result.json", 'rb'){|f| JSON.parse(f.read)}
+    schema = File.open('schema/result.json', 'rb'){|f| JSON.parse(f.read)}
     data = File.open('fixtures/result_example.json', 'rb'){|f| JSON.parse(f.read)}
     JSON::Schema.validate(data, schema)
   end
