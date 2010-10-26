1daca18bffc4eb2813ee1014e0a92dd864dd1a7f
Marc Hadley <mhadley@mitre.org>
2010-10-26 11:10:12 -0400

    Refactor some repetitive code, use test fixture instead of inline string constant.

--- spec/qme/map/map_reduce_builder_spec.rb
+++ spec/qme/map/map_reduce_builder_spec.rb
@@ -1,42 +1,3 @@
-COMPLEX_MEASURE_JSON = <<END_OF_COMPLEX_MEASURE
-{
-  "id": "0043",
-  "name": "Pneumonia Vaccination Status for Older Adults",
-  "steward": "NCQA",
-  "population": {
-    "and": [
-      {
-        "category": "Patient Characteristic",
-        "title": "Age > 17 before measure period",
-        "query": {"age": {"_gt": 17}}
-      },
-      {
-        "category": "Patient Characteristic",
-        "title": "Age < 75 before measure period",
-        "query": {"age": {"_lt": 75}}
-      },
-      {
-        "or": [
-          {
-            "category": "Patient Characteristic",
-            "title": "Male",
-            "query": {"sex": "male"}
-          },
-          {
-            "category": "Patient Characteristic",
-            "title": "Female",
-            "query": {"sex": "female"}
-          }
-        ]
-      }
-    ]
-  },
-  "denominator": {},
-  "numerator": {},
-  "exception": {}
-}
-END_OF_COMPLEX_MEASURE
-
 MAP_FUNCTION = <<END_OF_MAP_FN
 function () {
   var value = {i: 0, d: 0, n: 0, e: 0};
@@ -57,40 +18,38 @@ function () {
 END_OF_MAP_FN
 
 describe QME::MapReduce::Builder do
+  
+  before do
+    raw_measure_json = File.read('measures/0043/0043_NQF_PneumoniaVaccinationStatusForOlderAdults.json')
+    @measure_json = JSON.parse(raw_measure_json)
+    raw_measure_json = File.read('fixtures/complex_measure.json')
+    @complex_measure_json = JSON.parse(raw_measure_json)
+  end
+
   it 'should extract the measure metadata' do
-    measure_json = File.read('measures/0043/0043_NQF_PneumoniaVaccinationStatusForOlderAdults.json')
-    hash = JSON.parse(measure_json)
-    measure = QME::MapReduce::Builder.new(hash, :effective_date=>Time.gm(2010, 9, 19).to_i)
+    measure = QME::MapReduce::Builder.new(@measure_json, :effective_date=>Time.gm(2010, 9, 19).to_i)
     measure.id.should eql('0043')
   end
   it 'should extract three parameters for measure 0043 (one provided, two calculated)' do
-    measure_json = File.read('measures/0043/0043_NQF_PneumoniaVaccinationStatusForOlderAdults.json')
-    hash = JSON.parse(measure_json)
     time = Time.gm(2010, 9, 19).to_i
 
-    measure = QME::MapReduce::Builder.new(hash, :effective_date=>time)
+    measure = QME::MapReduce::Builder.new(@measure_json, :effective_date=>time)
     measure.parameters.size.should eql(3)
     measure.parameters.should have_key(:effective_date)
     measure.parameters[:effective_date].should eql(time)
   end
   it 'should raise a RuntimeError if not passed all the parameters' do
-    measure_json = File.read('measures/0043/0043_NQF_PneumoniaVaccinationStatusForOlderAdults.json')
-    hash = JSON.parse(measure_json)
-    lambda { QME::MapReduce::Builder.new(hash) }.should
+    lambda { QME::MapReduce::Builder.new(@measure_json) }.should
       raise_error(RuntimeError, 'No value supplied for measure parameter: effective_date')
   end
   it 'should calculate the calculated dates correctly' do
-    measure_json = File.read('measures/0043/0043_NQF_PneumoniaVaccinationStatusForOlderAdults.json')
-    hash = JSON.parse(measure_json)
     date = Time.gm(2010, 9, 19).to_i
-    measure = QME::MapReduce::Builder.new(hash, :effective_date=>date)
+    measure = QME::MapReduce::Builder.new(@measure_json, :effective_date=>date)
     measure.parameters[:earliest_encounter].should eql(date-QME::MapReduce::Builder::YEAR_IN_SECONDS)
   end
   it 'should produce valid JavaScript expressions for the query components' do
-    measure_json = File.read('measures/0043/0043_NQF_PneumoniaVaccinationStatusForOlderAdults.json')
-    hash = JSON.parse(measure_json)
     date = Time.gm(2010, 9, 19).to_i
-    builder = QME::MapReduce::Builder.new(hash, :effective_date=>date)
+    builder = QME::MapReduce::Builder.new(@measure_json, :effective_date=>date)
     builder.numerator.should eql('(this.measures["0043"].vaccination==true)')
     builder.denominator.should eql('(this.measures["0043"].encounter>='+builder.parameters[:earliest_encounter].to_s+')')
     builder.population.should eql('(this.birthdate<='+builder.parameters[:earliest_birthdate].to_s+')')
@@ -99,8 +58,7 @@ describe QME::MapReduce::Builder do
     builder.reduce_function.should eql(QME::MapReduce::Builder::REDUCE_FUNCTION)
   end
   it 'should handle logical combinations' do
-    hash = JSON.parse(COMPLEX_MEASURE_JSON)
-    builder = QME::MapReduce::Builder.new(hash, {})
+    builder = QME::MapReduce::Builder.new(@complex_measure_json, {})
     builder.population.should eql('((this.measures["0043"].age>17)&&(this.measures["0043"].age<75)&&((this.measures["0043"].sex=="male")||(this.measures["0043"].sex=="female")))')
   end
 end
