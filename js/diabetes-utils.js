a0ff077b92b94db9ffede110ad714a112d652fcb
Rob McCready <rob@mitre.org>
2010-12-13 11:56:47 -0500

    cleaned up some of the diabetes utility code, and fixed some comments

--- js/diabetes-utils.js
+++ js/diabetes-utils.js
@@ -1,5 +1,6 @@
-// Adds common utility functions to the root JS object. These are then
-// available for use by the map-reduce functions for each measure.
+// Adds diabetes utility functions to the root JS object. These are then
+// available for use by the supporting map-reduce functions for any measure
+// that needs common definitions of diabetes-specific algorithms.
 // lib/qme/mongo_helpers.rb executes this function on a database
 // connection.
 (function() {
@@ -26,7 +27,6 @@
     diagnosis_diabetes =            inRange(measure.diagnosis_diabetes, earliest_diagnosis, effective_date);
     encounter_acute_inpatient =     inRange(measure.encounter_acute_inpatient, earliest_diagnosis, effective_date);
     encounter_non_acute_inpatient = inRange(measure.encounter_non_acute_inpatient, earliest_diagnosis, effective_date);
-
     return (has_medications_indicative_of_diabetes(measure, earliest_diagnosis, effective_date) 
             || 
             (diagnosis_diabetes && (encounter_acute_inpatient || encounter_non_acute_inpatient == 2)));
@@ -35,7 +35,6 @@
   root.diabetes_exclusions = function(measure, earliest_diagnosis, effective_date) {
     diagnosis_gestational_diabetes =        inRange(measure.diagnosis_gestational_diabetes, earliest_diagnosis, effective_date);
     diagnosis_steroid_induced_diabetes =    inRange(measure.diagnosis_steroid_induced_diabetes, earliest_diagnosis, effective_date);
-
     return ((measure.polycystic_ovaries && !(diagnosis_diabetes && (encounter_acute_inpatient || encounter_non_acute_inpatient)))
             ||
             ((diagnosis_gestational_diabetes || diagnosis_steroid_induced_diabetes) 
