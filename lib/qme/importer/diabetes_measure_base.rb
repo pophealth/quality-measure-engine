b594e5d5c2b8f2ff1b23edcedaec247a4d74256a
Rob McCready <rob@mitre.org>
2010-12-17 14:28:24 -0500

    pulled out the diabetes C32 XML parsing logic into the generic diabetes parser

--- lib/qme/importer/diabetes_measure_base.rb
+++ lib/qme/importer/diabetes_measure_base.rb
@@ -23,6 +23,29 @@ module QME
     class DiabetesMeasureBase < MeasureBase
 
       def parse_diabetes_measure(doc, measure_info)
+        encounter_elements = doc.xpath("//cda:section[cda:templateId/@root='2.16.840.1.113883.3.88.11.83.127']/cda:entry/cda:encounter")
+        encounter_elements.each do |encounter_element|
+          create_property_from_code(encounter_element, "./cda:code", 'encounter_acute_inpatient',                     measure_info)
+          create_property_from_code(encounter_element, "./cda:code", 'encounter_non_acute_inpatient',                 measure_info)
+          create_property_from_code(encounter_element, "./cda:code", 'encounter_outpatient',                          measure_info)
+          create_property_from_code(encounter_element, "./cda:code", 'encounter_outpatient_opthamological_services',  measure_info)
+        end
+    
+        sa_elements = doc.xpath("//cda:section[cda:templateId/@root='2.16.840.1.113883.3.88.11.83.112']/cda:entry/cda:substanceAdministration")
+        sa_elements.each do |sa_element|
+          create_property_from_code(sa_element, 
+                                    "./cda:consumable/cda:manufacturedProduct/cda:manufacturedMaterial/cda:code",
+                                    'medications_indicative_of_diabetes', 
+                                    measure_info)
+        end
+    
+        problem_elements = doc.xpath("//cda:section[cda:templateId/@root='2.16.840.1.113883.3.88.11.83.103']/cda:entry/cda:act/cda:entryRelationship/cda:observation")
+        problem_elements.each do |problem_element|
+          create_property_from_code(problem_element, "./cda:value", 'diagnosis_diabetes',                 measure_info)
+          create_property_from_code(problem_element, "./cda:value", 'diagnosis_gestational_diabetes',     measure_info)
+          create_property_from_code(problem_element, "./cda:value", 'diagnosis_steroid_induced_diabetes', measure_info)
+          create_property_from_code(problem_element, "./cda:value", 'polycystic_ovaries',                 measure_info)
+        end
       end
 
     end
