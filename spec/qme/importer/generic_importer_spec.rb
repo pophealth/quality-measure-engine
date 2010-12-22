ae8fb1ea189b6ef7fcc58f428a7a048726fb8833
Andy Gregorowicz <andy@mitre.org>
2010-12-22 12:27:24 -0500

    Updating the NQF 0028 measure definition to work with the GenericImporter

--- spec/qme/importer/generic_importer_spec.rb
+++ spec/qme/importer/generic_importer_spec.rb
@@ -82,4 +82,18 @@ describe QME::Importer::GenericImporter do
     measure_info['diastolic_blood_pressure'].should include(942537600)
     measure_info['systolic_blood_pressure'].should include(942537600)
   end
+
+  it "should import the the information relevant to determining tobacco use" do
+    doc = Nokogiri::XML(File.new('fixtures/c32_fragments/0028/numerator.xml'))
+    doc.root.add_namespace_definition('cda', 'urn:hl7-org:v3')
+    patient = {}
+
+    gi = QME::Importer::GenericImporter.new(measure_definition(@loader, '0028'))
+    measure_info = gi.parse(doc)
+
+    measure_info['individual_counseling_encounter'].should include(1270598400)
+    measure_info['tobacco_user'].should include(1262304000)
+    measure_info['cessation_agent'].should include(1248825600)
+    measure_info['cessation_counseling'].should include(1252454400)
+  end
 end
