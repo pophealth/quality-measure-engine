0d93688bd40405c101f6c4e2b8d44db32d966ed6
Rob McCready <rob@mitre.org>
2010-12-22 10:55:05 -0500

    First pass at the 0038 mapper for IPV vaccine report

--- measures/0038/components/dtap_vaccine_map.js
+++ measures/0038/components/dtap_vaccine_map.js
@@ -30,19 +30,22 @@ function () {
     number_dtap_vaccine_administered = inRange(measure.dtap_vaccine_administrered, 
                                                earliest_dtap_vaccine, 
                                                latest_dtap_vaccine);
+
     // patient needs 4 different Tetanus and Acellular Pertussis (DTAP) vaccines
     // from the time that they are 42 days old, until the time that they are 2 years old
     return ((number_dtap_vaccine_administered >= 4) && 
-            // patient cannot have either DTaP vaccine allergy 
+            // patient cannot have either:
+            // DTaP vaccine allergy 
             // OR a diagnosis of encephalopathy
             // OR diagnosis of progressive neurological disorder
+            //
             // NOTE that this might belong in the exclusion logic
             // and will be discussed with NCQA in the future
-            !(inRange(measure.dtap_vaccine_allergy,            patient.birthdate, effective_date) 
+            !((inRange(measure.dtap_vaccine_allergy,            patient.birthdate, effective_date))
               ||
-             (inRange(measure.diagnosis_of_encephalopathy,     patient.birthdate, effective_date))
+              (inRange(measure.diagnosis_of_encephalopathy,     patient.birthdate, effective_date))
               ||
-             (inRange(measure.progressive_neurologic_disorder, patient.birthdate, effective_date))));
+              (inRange(measure.progressive_neurologic_disorder, patient.birthdate, effective_date))));
   }
 
   // no exclusions defined for any reports that are a part of NQF 0038
