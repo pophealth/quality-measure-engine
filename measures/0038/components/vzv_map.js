e49264b50bd1a99260402e9a2f7b7e5c16baa1bb
Rob McCready <rob@mitre.org>
2010-12-29 15:46:42 -0500

    Support for 0038 and the VZV chicken pox test

---
+++ measures/0038/components/vzv_map.js
@@ -0,0 +1,66 @@
+function () {
+  var patient = this;
+  var measure = patient.measures["0038"];
+  if (measure==null)
+    measure={};
+
+  var year = 365 * 24 * 60 * 60;
+  var effective_date =  <%= effective_date %>;
+  var earliest_birthdate =  effective_date - 2 * year;
+  var latest_birthdate =    effective_date - 1 * year;
+
+  // dtap vaccines are considered when they are occurring < 2 years after 
+  // the patients' birthdate
+  var latest_vzv_vaccine = patient.birthdate + 2 * year;
+
+  var population = function() {
+    return inRange(patient.birthdate, earliest_birthdate, latest_birthdate);
+  }
+
+  // the denominator logic is the same for all of the 0038 reports and this 
+  // code is defined in the shared library in the project in the code from 
+  // /js/childhood_immunizations.js
+  var denominator = function() {
+    return has_outpatient_encounter_with_pcp_obgyn(measure, patient.birthdate, effective_date);
+  }
+
+  var numerator = function() {
+    number_vzv_vaccine_administered = inRange(measure.vzv_vaccine_administered,
+                                              patient.birthdate,
+                                              latest_vzv_vaccine);
+
+    // To meet the criteria for this report, the patient needs to have either:
+    // 1 Chicken Pox (VZV) vaccine up until the time that they are 2 years old,
+    // OR resolution on VZV diagnosis by the end of the effective date of this measure
+    //
+    // AND the patients cannot have either:
+    // Lymphoreticular or Histiocytic cancer
+    // OR Asymptomatic HIV
+    // OR Multiple Myeloma
+    // OR Leukemia
+    // OR Immunodeficiency
+    // OR Medication allergy to VZV vaccine
+    return (((number_vzv_vaccine_administered >= 1)
+             ||
+             (conditionResolved(measure.vzv_diagnosis, patient.birthdate, effective_date)))
+            &&
+            !(inRange(measure.diagnosis_of_cancer_of_lymphoreticular_or_histiocytic_tissue, patient.birthdate, effective_date)
+              ||
+              inRange(measure.diagnosis_of_asymptomatic_hiv,                                patient.birthdate, effective_date)
+              ||
+              inRange(measure.multiple_myeloma_diagnosis,                                   patient.birthdate, effective_date)
+              ||
+              inRange(measure.leukemia_diagnosis,                                           patient.birthdate, effective_date)
+              ||
+              inRange(measure.immunodeficiency_diagnosis,                                   patient.birthdate, effective_date)
+              ||
+              inRange(measure.vzv_vaccine_allergy,                                          patient.birthdate, effective_date)));
+  }
+
+  // no exclusions defined for any reports that are a part of NQF 0038
+  var exclusion = function() {
+    return false;
+  }
+
+  map(patient, population, denominator, numerator, exclusion);
+};
--- spec/qme/importer/generic_importer_spec.rb
+++ spec/qme/importer/generic_importer_spec.rb
@@ -20,7 +20,7 @@ describe QME::Importer::GenericImporter do
   it "should import the information relevant to determining high blood pressure" do
     doc = Nokogiri::XML(File.new('fixtures/c32_fragments/0018/numerator.xml'))
     doc.root.add_namespace_definition('cda', 'urn:hl7-org:v3')
-    
+
     gi = QME::Importer::GenericImporter.new(measure_definition(@loader, '0018'))
     measure_info = gi.parse(doc)
 
@@ -49,7 +49,7 @@ describe QME::Importer::GenericImporter do
   it "should import the the information relevant to determining cervical cancer screening status" do
     doc = Nokogiri::XML(File.new('fixtures/c32_fragments/0032/numerator.xml'))
     doc.root.add_namespace_definition('cda', 'urn:hl7-org:v3')
-    
+
     gi = QME::Importer::GenericImporter.new(measure_definition(@loader, '0032'))
     measure_info = gi.parse(doc)
 
@@ -97,14 +97,11 @@ describe QME::Importer::GenericImporter do
     measure_info['encounter_non_acute_inpatient'].should include(1275177600)
     measure_info['encounter_outpatient'].should include(1275177600)
     measure_info['encounter_outpatient_opthamological_services'].should include(1275177600)
-    
     measure_info['medications_indicative_of_diabetes'].should include(1275177600)
-
     measure_info['diagnosis_diabetes'].should include(1275177600)
     measure_info['diagnosis_gestational_diabetes'].should include(1275177600)
     measure_info['diagnosis_steroid_induced_diabetes'].should include(1275177600)
     measure_info['polycystic_ovaries'].should include(1275177600)
-    
     measure_info['proceedure_eye_exam'].should include(1275177600)
   end
 
