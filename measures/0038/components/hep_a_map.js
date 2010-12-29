ade5a9b080e1265be899255a45198f3d56b07e63
Rob McCready <rob@mitre.org>
2010-12-29 18:48:01 -0500

    Hepatitis A report for NQF 0038

--- measures/0038/components/hep_a_map.js
+++ measures/0038/components/hep_a_map.js
@@ -9,9 +9,9 @@ function () {
   var earliest_birthdate =  effective_date - 2 * year;
   var latest_birthdate =    effective_date - 1 * year;
 
-  // Hepatitis B vaccines are considered when they are occurring < 2 years after
+  // Hepatitis A vaccines are considered when they are occurring < 2 years after
   // the patients' birthdate
-  var latest_hep_b_vaccine = patient.birthdate + 2 * year;
+  var latest_hep_a_vaccine = patient.birthdate + 2 * year;
 
   var population = function() {
     return inRange(patient.birthdate, earliest_birthdate, latest_birthdate);
@@ -25,23 +25,19 @@ function () {
   }
 
   var numerator = function() {
-    number_hep_b_vaccine_administered = inRange(measure.hepatitis_b_vaccine_administered,
+    number_hep_a_vaccine_administered = inRange(measure.hepatitis_a_vaccine_administered,
                                                 patient.birthdate,
-                                                latest_hep_b_vaccine);
+                                                latest_hep_a_vaccine);
 
     // To meet the criteria for this report, the patient needs either:
-    // 3 different Hepatitis B (Hep B) vaccines until the time that they are 2 years old,
-    // OR resolution on a hepatitis B diagnosis by the end of the effective date of this measure
-    //
-    // AND the patients cannot have either an allergy to either Baker's yeast, or an allergy 
-    // to Hepatitis B vaccine
-    return (((number_hep_b_vaccine_administered >= 3)
+    // 2 different Hepatitis A (Hep A) vaccines until the time that they are 2 years old,
+    // OR resolution on a hepatitis A diagnosis by the end of the effective date of this measure
+    // AND the patients cannot have an allergy to hepatitis A vaccine
+    return (((number_hep_a_vaccine_administered >= 2)
               ||
-             (conditionResolved(measure.hepatitis_b_diagnosis, patient.birthdate, effective_date)))
+             (conditionResolved(measure.hepatitis_a_diagnosis, patient.birthdate, effective_date)))
             &&
-            !(inRange(measure.allergy_to_bakers_yeast,         patient.birthdate, effective_date)
-              ||
-              inRange(measure.hepatitis_b_vaccine_allergy,     patient.birthdate, effective_date)));
+            !(inRange(measure.hepatitis_a_vaccine_allergy,     patient.birthdate, effective_date)));
   }
 
   // no exclusions defined for any reports that are a part of NQF 0038
