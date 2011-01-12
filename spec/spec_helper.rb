c3e6493cf663ecbd168d8c0d14a2325e9d183e2e
Rob Dingwell <rob@robdingwell.org>
2011-01-12 13:45:42 -0500

    removing extraneous @ sign

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -49,7 +49,7 @@ def validate_measures(measure_dirs, loader)
       expected = JSON.parse(File.read(result_file))
       
       # evaulate measure using Map/Reduce and validate results
-      executor = QME::MapReduce::Executor.new(@loader.get_db)
+      executor = QME::MapReduce::Executor.new(loader.get_db)
       measures.each do |measure|
         measure_id = measure['id']
         sub_id = measure['sub_id']
