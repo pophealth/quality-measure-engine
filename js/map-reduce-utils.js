1f5246032ac081eb14ad199c4e64a9b15cd75015
Marc Hadley <mhadley@mitre.org>
2011-03-22 12:08:51 -0400

    Remove duplicated functionality

--- js/map-reduce-utils.js
+++ js/map-reduce-utils.js
@@ -92,6 +92,8 @@
   root.actionFollowingSomething = function(something, action, timePeriod) {
     something = normalize(something);
     action = normalize(action);
+    if (timePeriod===undefined)
+      timePeriod=Infinity;
     
     var result = 0;
     for (i=0; i<something.length; i++) {
@@ -109,18 +111,7 @@
   // something. The first two arguments are arrays or single-valued time stamps in
   // seconds-since-the-epoch.
   root.actionAfterSomething = function(something, action) {
-    something = normalize(something);
-    action = normalize(action);
-   
-    var result = 0;
-    for (i=0; i<something.length; i++) {
-      var timeStamp = something[i];
-      for (j=0; j<action.length;j++) {
-        if (action[j]>=timeStamp )
-          result++;
-      }
-    }
-    return result;
+    return actionFollowingSomething(something, action);
   }
   
   // Returns all members of the values array that fall between min and max inclusive
