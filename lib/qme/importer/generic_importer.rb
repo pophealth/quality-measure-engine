185ab521a4dab1b366cbaf10ca1c4e8000839bd9
Marc Hadley <mhadley@mitre.org>
2011-03-25 10:43:15 -0400

    Suppress multiple warning for same problem

--- lib/qme/importer/generic_importer.rb
+++ lib/qme/importer/generic_importer.rb
@@ -92,6 +92,7 @@ module QME
         else
           unless @warnings.include?(standard_category)
             puts "Warning: Unsupported standard_category (#{standard_category})"
+            @warnings << standard_category
           end
           []
         end
