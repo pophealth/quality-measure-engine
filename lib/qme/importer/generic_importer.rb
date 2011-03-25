e514677f0676c9cd2f4aa7a5a16ce56273c1dd14
Andy Gregorowicz <andy@mitre.org>
2011-03-25 10:49:57 -0400

    Now filtering conditions based on status as given by the qds_data_type

--- lib/qme/importer/generic_importer.rb
+++ lib/qme/importer/generic_importer.rb
@@ -23,16 +23,11 @@ module QME
         @definition['measure'].each_pair do |property, description|
           raise "No standard_category for #{property}" if !description['standard_category']
           matcher = PropertyMatcher.new(description)
-          entry_list = symbols_for_property(description['standard_category'], description['qds_data_type']).map do |section|
-            if patient_hash[section]
-              patient_hash[section]
-            else
-              []
-            end
-          end.flatten
+          enrty_filter = filter_for_property(description['standard_category'], description['qds_data_type'])
+          entry_list = enrty_filter.call(patient_hash)
           if ! entry_list.empty?
             matched_list = matcher.match(entry_list)
-            measure_info[property]=matched_list if matched_list.length>0
+            measure_info[property] = matched_list if matched_list.length > 0
           end
         end
         
@@ -41,59 +36,88 @@ module QME
       
       private
       
-      def symbols_for_property(standard_category, qds_data_type)
+      def create_section_filter(*sections)
+        Proc.new do |patient_hash|
+          sections.map do |section|
+            if patient_hash[section]
+              patient_hash[section]
+            else
+              []
+            end
+          end.flatten
+        end
+      end
+      
+      def create_section_and_status_filter(status, *sections)
+        section_filter = create_section_filter(*sections)
+        Proc.new do |patient_hash|
+          entry_list = section_filter.call(patient_hash)
+          entry_list.select do |entry|
+            entry.status.nil? || entry.status == status
+          end
+        end
+      end
+      
+      def filter_for_property(standard_category, qds_data_type)
         # Currently unsupported categories: negation_rationale, risk_category_assessment
         case standard_category
         when 'encounter'
-          [:encounters]
+          create_section_filter(:encounters)
         when 'procedure'
           case qds_data_type
           when 'procedure_performed'
-            [:procedures]
+            create_section_filter(:procedures)
           when 'procedure_adverse_event', 'procedure_intolerance'
-            [:allergies]
+            create_section_filter(:allergies)
           when 'procedure_result'
-            [:procedures, :results, :vital_signs]
+            create_section_filter(:procedures, :results, :vital_signs)
           end
         when 'risk_category_assessment'
-          [:procedures]
+          create_section_filter(:procedures)
         when 'communication'
-          [:procedures]
+          create_section_filter(:procedures)
         when 'laboratory_test'
-          [:results, :vital_signs]
+          create_section_filter(:results, :vital_signs)
         when 'physical_exam'
-          [:vital_signs]
+          create_section_filter(:vital_signs)
         when 'medication'
           case qds_data_type
           when 'medication_dispensed', 'medication_order', 'medication_active', 'medication_administered'
-            [:medications]
+            create_section_filter(:medications)
           when 'medication_allergy', 'medication_intolerance', 'medication_adverse_event'
-            [:allergies]
+            create_section_filter(:allergies)
           end
         when 'diagnosis_condition_problem'
-          [:conditions, :social_history]
+          case qds_data_type
+          when 'diagnosis_active'
+            create_section_and_status_filter(:active, :conditions, :social_history)
+          when 'diagnosis_inactive'
+            create_section_and_status_filter(:inactive, :conditions, :social_history)
+          when 'diagnosis_resolved'
+            create_section_and_status_filter(:resolved, :conditions, :social_history)
+          end
         when 'symptom'
-          [:conditions, :social_history]
+          create_section_filter(:conditions, :social_history)
         when 'individual_characteristic'
-          [:conditions, :social_history]
+          create_section_filter(:conditions, :social_history)
         when 'device'
           case qds_data_type
           when 'device_applied'
-            [:conditions, :procedures, :care_goals, :medical_equipment]
+            create_section_filter(:conditions, :procedures, :care_goals, :medical_equipment)
           when 'device_allergy'
-            [:allergies]
+            create_section_filter(:allergies)
           end
         when 'care_goal'
-          [:care_goals]
+          create_section_filter(:care_goals)
         when 'diagnostic_study'
-          [:procedures]
+          create_section_filter(:procedures)
         when 'substance'
-          [:allergies]
+          create_section_filter(:allergies)
         else
           unless @warnings.include?(standard_category)
             puts "Warning: Unsupported standard_category (#{standard_category})"
           end
-          []
+          Proc.new {[]} # A proc that returns an empty array
         end
       end
     end
