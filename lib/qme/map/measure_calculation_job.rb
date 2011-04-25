abee6398a1eb540b4bfe46917583e8ab3512d02f
Andy Gregorowicz <andy@mitre.org>
2011-04-25 10:34:34 -0400

    Now checking if a measure has been calculated before running the job. Should prevent double calculation for jobs that have been added twice.

--- lib/qme/map/measure_calculation_job.rb
+++ lib/qme/map/measure_calculation_job.rb
@@ -1,15 +1,19 @@
 module QME
   module MapReduce
     class MeasureCalculationJob < Resque::JobWithStatus
-      
       def perform
-        map = QME::MapReduce::Executor.new(options['measure_id'], options['sub_id'], 'effective_date' => options['effective_date'])
-        tick('Starting MapReduce')
-        map.map_records_into_measure_groups
-        tick('MapReduce complete')
-        tick('Calculating group totals')
-        result = map.count_records_in_measure_groups
-        completed("#{options['measure_id']}#{options['sub_id']}: p#{result['population']}, d#{result['denominator']}, n#{result['numerator']}, e#{result['exclusions']}")
+        qr = QualityReport.new(options['measure_id'], options['sub_id'], 'effective_date' => options['effective_date'])
+        if qr.calculated?
+          completed("#{options['measure_id']}#{options['sub_id']} has already been calculated")
+        else
+          map = QME::MapReduce::Executor.new(options['measure_id'], options['sub_id'], 'effective_date' => options['effective_date'])
+          tick('Starting MapReduce')
+          map.map_records_into_measure_groups
+          tick('MapReduce complete')
+          tick('Calculating group totals')
+          result = map.count_records_in_measure_groups
+          completed("#{options['measure_id']}#{options['sub_id']}: p#{result['population']}, d#{result['denominator']}, n#{result['numerator']}, e#{result['exclusions']}")
+        end
       end
     end
   end
