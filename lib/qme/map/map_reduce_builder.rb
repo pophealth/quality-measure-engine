e21c982471d0ed3c238afa434cdc748177244bd6
Marc Hadley <mhadley@mitre.org>
2011-05-20 10:04:48 -0400

    Remove extraneous debug

--- lib/qme/map/map_reduce_builder.rb
+++ lib/qme/map/map_reduce_builder.rb
@@ -86,7 +86,6 @@ module QME
           var patient = value;
           patient.measure_id = \"#{@measure_def['id']}\";\n"
         if @params['test_id'] && @params['test_id'].class==BSON::ObjectId
-          puts "test_id is '#{@params['test_id']}'"
           reduce += "  patient.test_id = new ObjectId(\"#{@params['test_id']}\");\n"
         end
         if @measure_def['sub_id']
