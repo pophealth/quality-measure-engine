ceed8b4bdbe59c19264dff0f989d7a70c8dd70dd
Andre Quina <aquina@mitre.org>
2011-12-14 21:27:22 -0500

    updated exclusion logic

--- lib/qme/map/map_reduce_executor.rb
+++ lib/qme/map/map_reduce_executor.rb
@@ -25,35 +25,41 @@ module QME
       # @return [Hash] measure groups (like numerator) as keys, counts as values
       def count_records_in_measure_groups
         patient_cache = get_db.collection('patient_cache')
-        query = {'value.measure_id' => @measure_id, 'value.sub_id' => @sub_id,
-                 'value.effective_date' => @parameter_values['effective_date'],
-                 'value.test_id' => @parameter_values['test_id']}
+        base_query = {'value.measure_id' => @measure_id, 'value.sub_id' => @sub_id,
+                      'value.effective_date' => @parameter_values['effective_date'],
+                      'value.test_id' => @parameter_values['test_id']}
+        
+        query = base_query.clone
         
         query.merge!(filter_parameters)
+        query.merge!({'value.manual_exclusion'=>{'$ne'=>true}})
         
         result = {:measure_id => @measure_id, :sub_id => @sub_id, 
                   :effective_date => @parameter_values['effective_date'],
                   :test_id => @parameter_values['test_id'], :filters => @parameter_values['filters']}
         
         aggregate = patient_cache.group({cond: query, 
-                                           initial: {population: 0, denominator: 0, numerator: 0, antinumerator: 0,  exclusions: 0, considered: 0}, 
-                                           reduce: "function(record,sums) {
-                                                      for (var key in sums) {
-                                                        if (!record['value']['manual_exclusion'])
-                                                          sums[key] += (record['value'][key] || key == 'considered') ? 1 : 0
-                                                      }
-                                                    }"}).first
-        
-        aggregate ||= {population: 0, denominator: 0, numerator: 0, antinumerator: 0,  exclusions: 0}
-        aggregate.each {|key, value| aggregate[key] = value.to_i}
-        result.merge!(aggregate)
-        
-# need to time the old way agains the single query to verify that the single query is more performant        
-#        %w(population denominator numerator antinumerator exclusions).each do |measure_group|
-#          patient_cache.find(query.merge("value.#{measure_group}" => true)) do |cursor|
-#            result[measure_group] = cursor.count
-#          end
-#        end
+                                            initial: {population: 0, denominator: 0, numerator: 0, antinumerator: 0,  exclusions: 0, considered: 0}, 
+                                            reduce: "function(record,sums) {
+                                                       for (var key in sums) {
+                                                           sums[key] += (record['value'][key] || key == 'considered') ? 1 : 0
+                                                       }
+                                                     }"}).first
+         
+         aggregate ||= {"population"=>0, "denominator"=>0, "numerator"=>0, "antinumerator"=>0, "exclusions"=>0}
+         aggregate.each {|key, value| aggregate[key] = value.to_i}
+         aggregate['exclusions'] += patient_cache.find(base_query.merge({'value.manual_exclusion'=>true})).count
+         result.merge!(aggregate)
+
+        # # need to time the old way agains the single query to verify that the single query is more performant        
+        # aggregate = {population: 0, denominator: 0, numerator: 0, antinumerator: 0,  exclusions: 0}
+        # %w(population denominator numerator antinumerator exclusions).each do |measure_group|
+        #   patient_cache.find(query.merge("value.#{measure_group}" => true)) do |cursor|
+        #     aggregate[measure_group] = cursor.count
+        #   end
+        # end
+        # aggregate[:considered] = patient_cache.find(query).count
+        # result.merge!(aggregate)
 
         result.merge!(execution_time: (Time.now.to_i - @parameter_values['start_time'].to_i)) if @parameter_values['start_time']
 
