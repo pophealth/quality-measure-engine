bc27940b1c9946f3cf70004ad9a54eed80c17483
Samuel Sayer <ssayer@mitre.org>
2012-02-15 11:38:03 -0500

    moved provider to health data standards

--- lib/quality-measure-engine.rb
+++ lib/quality-measure-engine.rb
@@ -22,7 +22,6 @@ require_relative 'qme/ext/record'
 
 require_relative 'qme/importer/property_matcher'
 require_relative 'qme/importer/generic_importer'
-require_relative 'qme/importer/provider_importer'
 require_relative 'qme/importer/measure_properties_generator'
 
 require 'json'
