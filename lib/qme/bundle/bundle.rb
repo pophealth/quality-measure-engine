fa657819f2f9e842f23baa0f03df70c6d3066acf
Adam Goldstein <agoldstein@mitre.org>
2012-09-24 12:28:48 -0400

    Making sure we persist _id from original export when importing.

--- lib/qme/bundle/bundle.rb
+++ lib/qme/bundle/bundle.rb
@@ -3,18 +3,18 @@ module QME
     # Delete a list of collections. By default, this function drops all of collections related to measures and patients.
     #
     # @param [Array] collection_names Optionally, an array of collection names to be dropped.
-    def self.drop_collections(collection_names=[])
+    def self.drop_collections(db, collection_names=[])
       collection_names = ["bundles", "records", "measures", "selected_measures", "patient_cache", "query_cache", "System.js"] if collection_names.empty?
-      collection_names.each {|collection| MONGO_DB[collection].drop}
+      collection_names.each {|collection| db[collection].drop}
     end
 
     # Save a javascript function into Mongo's system.js collection for measure execution.
     #
     # @param [String] name The name by which the function will be referred.
     # @param [String] fn The body of the function being saved.
-    def self.save_system_js_fn(name, fn)
+    def self.save_system_js_fn(db, name, fn)
       fn = "function () {\n #{fn} \n }"
-      MONGO_DB['system.js'].save(
+      db['system.js'].save(
         {
           "_id" => name,
           "value" => BSON::Code.new(fn)
