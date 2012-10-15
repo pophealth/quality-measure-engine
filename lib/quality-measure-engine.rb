5e859fc7d97ba87f78aba4ab22f9bf08d97a5237
Andy Gregorowicz <andy@mitre.org>
2012-10-15 14:23:27 -0400

    Autogenerated gem stub from Bundler

---
+++ lib/quality-measure-engine.rb
@@ -0,0 +1,9 @@
+require "quality-measure-engine/version"
+
+module Quality
+  module Measure
+    module Engine
+      # Your code goes here...
+    end
+  end
+end
---
+++ lib/quality-measure-engine/version.rb
@@ -0,0 +1,7 @@
+module Quality
+  module Measure
+    module Engine
+      VERSION = "0.0.1"
+    end
+  end
+end
