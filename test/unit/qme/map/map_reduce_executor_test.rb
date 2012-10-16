c4e1bba5c67d4f9ca63476597b4c6b4497bca22a
Andy Gregorowicz <andy@mitre.org>
2012-10-16 16:45:13 -0400

    further testing of map reduce execution

--- test/unit/qme/map/map_reduce_executor_test.rb
+++ test/unit/qme/map/map_reduce_executor_test.rb
@@ -19,4 +19,14 @@ class MapReduceExecutorTest < MiniTest::Unit::TestCase
     assert_equal 1, get_db['patient_cache'].find('value.population' => true).count
     assert_equal 1, get_db['patient_cache'].find('value.population' => false).count
   end
+
+  def test_count_records_in_measure_groups
+    executor = QME::MapReduce::Executor.new("2E679CD2-3FEC-4A75-A75A-61403E5EFEE8", nil,
+                                            'effective_date' => Time.gm(2012, 10, 16).to_i)
+    executor.map_records_into_measure_groups
+    executor.count_records_in_measure_groups
+    assert_equal 1, get_db['query_cache'].find().count
+    doc = get_db['query_cache'].find().first
+    assert_equal 1, doc['population']
+  end  
 end
