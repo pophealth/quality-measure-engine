f344a9bdd427631ce2ac1ef0e5478069c49a46ed
Andy Gregorowicz <andy@mitre.org>
2012-10-17 14:18:01 -0400

    Adding much more complete testing of the MapReduce measure execution

--- test/unit/qme/map/map_reduce_executor_test.rb
+++ test/unit/qme/map/map_reduce_executor_test.rb
@@ -12,21 +12,25 @@ class MapReduceExecutorTest < MiniTest::Unit::TestCase
 
   def test_map_records_into_measure_groups
     executor = QME::MapReduce::Executor.new("2E679CD2-3FEC-4A75-A75A-61403E5EFEE8", nil,
-                                            'effective_date' => Time.gm(2012, 10, 16).to_i)
+                                            'effective_date' => Time.gm(2011, 1, 15).to_i)
     executor.map_records_into_measure_groups
 
-    assert_equal 2, get_db['patient_cache'].find().count
-    assert_equal 1, get_db['patient_cache'].find('value.population' => true).count
+    assert_equal 4, get_db['patient_cache'].find().count
+    assert_equal 3, get_db['patient_cache'].find('value.population' => true).count
     assert_equal 1, get_db['patient_cache'].find('value.population' => false).count
+    assert_equal 2, get_db['patient_cache'].find('value.denominator' => true).count
+    assert_equal 1, get_db['patient_cache'].find('value.numerator' => true).count
   end
 
   def test_count_records_in_measure_groups
     executor = QME::MapReduce::Executor.new("2E679CD2-3FEC-4A75-A75A-61403E5EFEE8", nil,
-                                            'effective_date' => Time.gm(2012, 10, 16).to_i)
+                                            'effective_date' => Time.gm(2011, 1, 15).to_i)
     executor.map_records_into_measure_groups
     executor.count_records_in_measure_groups
     assert_equal 1, get_db['query_cache'].find().count
     doc = get_db['query_cache'].find().first
-    assert_equal 1, doc['population']
+    assert_equal 3, doc['population']
+    assert_equal 2, doc['denominator']
+    assert_equal 1, doc['numerator']
   end  
 end
