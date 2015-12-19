f7b1389c31f5763c87a78f7bf64ebe0d106513a0
Dillon Welch <daw0328@gmail.com>
2015-12-18 16:48:31 -0800

    Remove separate formatter.

--- test/simplecov_setup.rb
+++ test/simplecov_setup.rb
@@ -9,14 +9,3 @@ SimpleCov.start do
   add_group "Map Reduce", "lib/qme/map"
   add_group "Bundles", "lib/qme/bundle"
 end
-
-class SimpleCov::Formatter::QualityFormatter
-  def format(result)
-    SimpleCov::Formatter::HTMLFormatter.new.format(result)
-    File.open("coverage/covered_percent", "w") do |f|
-      f.puts result.source_files.covered_percent.to_f
-    end
-  end
-end
-
-SimpleCov.formatter = SimpleCov::Formatter::QualityFormatter
