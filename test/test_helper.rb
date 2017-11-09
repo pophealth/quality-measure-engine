b3f9e782c2936e8f3a3d4fad8107421447cde5b9
Samuel Sayer <ssayer@mitre.org>
2017-11-09 17:15:55 -0500

    fix fixture loading

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -23,18 +23,48 @@ class MiniTest::Unit::TestCase
 
   end
 
-  # Add more helper methods to be used by all tests here...
 
-  def collection_fixtures(db, collection, *id_attributes)
-    db[collection].drop
-    Dir.glob(File.join(File.dirname(__FILE__), 'fixtures', collection, '*.json')).each do |json_fixture_file|
-      #puts "Loading #{json_fixture_file}"
-      fixture_json = JSON.parse(File.read(json_fixture_file))
-      id_attributes.each do |attr|
-        fixture_json[attr] = BSON::ObjectId.from_string(fixture_json[attr])
+  def value_or_bson(v)
+    if v.is_a? Hash
+      if v['$oid']
+        BSON::ObjectId.from_string(v['$oid'])
+      else
+        map_bson_ids(v)
       end
+    else
+      v
+    end
+  end
 
-      db[collection].insert_one(fixture_json)
+  def map_array(arr)
+    ret = []
+    arr.each do |v|
+      ret << value_or_bson(v)
+    end
+    ret
+  end
+
+  def map_bson_ids(json)
+    json.each_pair do |k, v|
+      if v.is_a? Hash
+        json[k] = value_or_bson(v)
+      elsif v.is_a? Array
+        json[k] = map_array(v)
+      elsif k == 'create_at' || k == 'updated_at'
+        json[k] = Time.at.local(v).in_time_zone
+      end
+    end
+    json
+  end
+
+  def collection_fixtures(db, *collections)
+    collections.each do |collection|
+      db[collection].drop
+      Dir.glob(File.join(File.dirname(__FILE__), 'fixtures', collection, '*.json')).each do |json_fixture_file|
+        fixture_json = JSON.parse(File.read(json_fixture_file), max_nesting: 250)
+        map_bson_ids(fixture_json)
+        Mongoid.default_client[collection].insert_one(fixture_json)
+      end
     end
   end
 end
