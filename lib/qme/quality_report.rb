5550bf60c432cf9260717d6e054c6e4adc3eb70b
Giriraj Kashyap <giriraj.kashyapa@gmail.com>
2018-10-13 12:11:21 -0500

    Update quality_report.rb

--- lib/qme/quality_report.rb
+++ lib/qme/quality_report.rb
@@ -188,7 +188,9 @@ module QME
       puts "#############in patient cache matcher######################"
       measure_id = HealthDataStandards::CQM::Measure.where(hqmf_id: self.measure_id,).pluck(:_id).first.to_s
       puts measure_id
+      puts self.sub_id
       match = {'measure_id' => measure_id,
+               'sub_id' => self.sub_id,
                'extendedData.effective_date'   => Time.at(self.effective_date).in_time_zone.to_formatted_s(:number),
                'extendedData.manual_exclusion' => {'$in' => [nil, false]}              
               }
